2605259b8dd4216318091b95813483e485016c57
Jérémy Bézairie <jeremy.bezairie@gmail.com>
2026-01-21 11:51:33 +0000

    Add files via upload

---
+++ !.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @include https://www.youtube.com/watch?v=*
+// ==/UserScript==
+document.getElementsByClassName("ytp-overlays-container")[0].remove()
+document.getElementsByClassName("ytp-fullscreen-grid")[0].remove()
+a=document.getElementsByTagName("video")[0]
+a.addEventListener("pause",()=>document.getElementsByClassName("ytp-bezel-text-hide")[0].remove(),{once:true})
+b=document.body.style
+let $
+a.onmousemove=()=>{b.cursor=""
+clearTimeout($)
+$=setTimeout(()=>b.cursor="none",500)}
+a.onmouseleave=()=>setTimeout(()=>b.cursor="",500)
+c=document.getElementsByClassName("ytp-chrome-bottom")[0]
+d=c.style
+c.onmouseenter=()=>d.opacity=1
+c.onmouseleave=()=>d.opacity=0
